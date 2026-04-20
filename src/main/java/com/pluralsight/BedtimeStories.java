2e039123001c097e136f226c7789034e4d7e03cb
Nurbu <lamanurbu2@gmail.com>
2026-04-20 14:55:14 -0400

    Added comments.

--- src/main/java/com/pluralsight/BedtimeStories.java
+++ src/main/java/com/pluralsight/BedtimeStories.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class BedtimeStories {
+    // Gives available bedtime stories to read.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,18 +14,21 @@ public class BedtimeStories {
         System.out.println("hansel_and_gretel.txt");
         System.out.println("mary_had_a_little_lamb.txt");
         System.out.print("\nChoose a bed time story: ");
-        String bedTimeStory = scanner.nextLine();
+        // Takes user input and makes it lowercase
+        String bedTimeStory = scanner.nextLine().toLowerCase();
 
         try {
             FileReader fileReader = new FileReader(bedTimeStory);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
             int count = 1;
+            // Goes through buffer Line by Line and prints it.
             while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(count + ". " + line);
                 count++;
             }
         } catch (Exception e) {
+            // Displays error
             System.out.println(e.getMessage());
         }
 
