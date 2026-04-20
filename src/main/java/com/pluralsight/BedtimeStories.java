ca84bcadb789e9f163da5115fd3d362f4a27ca5c
Nurbu <lamanurbu2@gmail.com>
2026-04-20 14:48:10 -0400

    Finished asking user which story they'd like.

--- src/main/java/com/pluralsight/BedtimeStories.java
+++ src/main/java/com/pluralsight/BedtimeStories.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class BedtimeStories {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("List of bed time stories:");
+        System.out.println("\ngoldilocks.txt");
+        System.out.println("hansel_and_gretel.txt");
+        System.out.println("mary_had_a_little_lamb.txt");
+        System.out.print("Choose a bed time story: ");
+        String bedTimeStory = scanner.nextLine();
+
+
+    }
 }
